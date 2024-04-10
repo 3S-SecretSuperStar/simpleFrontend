9943dc7f12f8c8c859ffde0aef88158bd348bbe1
trinklestar58 <sweetheart199558@gmail.com>
2024-04-11 04:23:25 +1000

    fix style

--- index.js
+++ index.js
@@ -1,37 +1,27 @@
-const assignGlobal = name => (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name];
+(function(document) {
+  var metas = document.getElementsByTagName('meta'),
+      changeViewportContent = function(content) {
+          for (var i = 0; i < metas.length; i++) {
+              if (metas[i].name == "viewport") {
+                  metas[i].content = content;
+              }
+          }
+      },
+      initialize = function() {
+          changeViewportContent("width=device-width, minimum-scale=1.0, maximum-scale=1.0");
+      },
+      gestureStart = function() {
+          changeViewportContent("width=device-width, minimum-scale=0.25, maximum-scale=1.6");
+      },
+      gestureEnd = function() {
+          initialize();
+      };
 
-if (!self.__WB_pmw) { 
-  self.__WB_pmw = obj => { 
-    this.__WB_source = obj; 
-    return this; 
-  } 
-}
 
-const window = assignGlobal("window");
-const self = assignGlobal("self");
-const document = assignGlobal("document");
-const location = assignGlobal("location");
-const top = assignGlobal("top");
-const parent = assignGlobal("parent");
-const frames = assignGlobal("frames");
-const opener = assignGlobal("opener");
+  if (navigator.userAgent.match(/iPhone/i)) {
+      initialize();
 
-const metas = document.getElementsByTagName('meta');
-const isiPhone = navigator.userAgent.match(/iPhone/i);
-
-if (isiPhone) {
-  for (let i = 0; i < metas.length; i++) {
-    if (metas[i].name === "viewport") {
-      metas[i].content = "width=device-width, minimum-scale=1.0, maximum-scale=1.0";
-    }
+      document.addEventListener("touchstart", gestureStart, false);
+      document.addEventListener("touchend", gestureEnd, false);
   }
-  document.addEventListener("gesturestart", gestureStart, false);
-}
-
-function gestureStart() {
-  for (let i = 0; i < metas.length; i++) {
-    if (metas[i].name === "viewport") {
-      metas[i].content = "width=device-width, minimum-scale=0.25, maximum-scale=1.6";
-    }
-  }
-}
+})(document);
